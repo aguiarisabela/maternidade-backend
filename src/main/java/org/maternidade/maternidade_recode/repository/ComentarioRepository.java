dae022dc9317139b95b0ea8c0d3afb792d3ff0b2
AndiaraRios <andiara.cel@gmail.com>
2025-06-15 20:52:05 -0300

    Backend sincronizado com frontend

--- src/main/java/org/maternidade/maternidade_recode/repository/ComentarioRepository.java
+++ src/main/java/org/maternidade/maternidade_recode/repository/ComentarioRepository.java
@@ -1,7 +1,10 @@
 package org.maternidade.maternidade_recode.repository;
 
+import java.util.List;
+
 import org.maternidade.maternidade_recode.model.Comentario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ComentarioRepository extends JpaRepository<Comentario, Long> {
+    List<Comentario> findByPostId(Long postId);
 }

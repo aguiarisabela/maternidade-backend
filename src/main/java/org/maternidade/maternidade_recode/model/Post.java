459c525d5ee3ef4bcdc056a8cbd10db6cb1f36ff
Fernandaaline <fernanda.aline3583@gmail.com>
2025-06-15 19:48:21 -0300

    Atualizando o back end Post

--- src/main/java/org/maternidade/maternidade_recode/model/Post.java
+++ src/main/java/org/maternidade/maternidade_recode/model/Post.java
@@ -1,26 +1,71 @@
 package org.maternidade.maternidade_recode.model;
 
 import jakarta.persistence.*;
-import lombok.*;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Entity
-@Data
-@NoArgsConstructor 
-@AllArgsConstructor 
-@Builder
-@Table(name = "posts")
+@Table(name = "Post") // Nome da tabela no banco
 public class Post {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "titulo")
     private String titulo;
+
+    @Column(name = "conteudo")
     private String conteudo;
+
+    @Column(name = "dataCriacao")
     private LocalDateTime dataCriacao;
 
-    @ManyToOne
+    @ManyToOne // Relacionamento: um post tem um autor
+    @JoinColumn(name = "autorId") // Nome da coluna no banco
     private User autor;
 
-    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
-    private List<Comentario> comentarios;
+    // Construtor vazio (necessário para JPA)
+    public Post() {
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public User getAutor() {
+        return autor;
+    }
+
+    public void setAutor(User autor) {
+        this.autor = autor;
+    }
 }

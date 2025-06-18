e15ca660291249230dc12d02e43ad42d2d83b739
Isabela Aguiar <isilav.is54@gmail.com>
2025-06-17 21:17:29 -0300

    Removendo códigos desnecessários do 'MaternidadeController.java'

--- src/main/java/org/maternidade/maternidade_recode/controller/MaternidadeController.java
+++ src/main/java/org/maternidade/maternidade_recode/controller/MaternidadeController.java
@@ -1,5 +1,5 @@
-
 package org.maternidade.maternidade_recode.controller;
+
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.Optional;
 import org.maternidade.maternidade_recode.model.User;
 import org.maternidade.maternidade_recode.service.UserService;
-
 
 @RestController
 @RequestMapping("/api")
@@ -23,24 +22,8 @@ public class MaternidadeController {
         return "comunidade";
     }
 
-     @Autowired
+    @Autowired
     private UserService userService;
-
-    // @PostMapping("/login")
-    // public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
-    //     String identifier = loginRequest.getUsername();
-    //     String password = loginRequest.getPassword();
-
-    //     Optional<User> userOptional = userService.findByUsername(identifier);
-    //     if (!userOptional.isPresent()) {
-    //         userOptional = userService.findByEmail(identifier);
-    //     }
-    //     if (userOptional.isPresent() && userOptional.get().getSenha().equals(password)) {
-    //         return ResponseEntity.ok("Login bem-sucedido!");
-    //     } else {
-    //         return ResponseEntity.badRequest().body("Usuário ou senha inválidos!");
-    //     }
-    // }
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody User user) {
@@ -60,27 +43,5 @@ public class MaternidadeController {
         }
         return userOptional.map(user -> ResponseEntity.ok(user))
                 .orElseGet(() -> ResponseEntity.notFound().build());
-    }
-
-    public static class LoginRequest {
-
-        private String username;
-        private String password;
-
-        public String getUsername() {
-            return username;
-        }
-
-        public void setUsername(String username) {
-            this.username = username;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
     }
 }
